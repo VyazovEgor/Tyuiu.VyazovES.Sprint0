ad50bc69f6548007357a1e31937ba3bf2661918e
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-09-25 00:15:23 +0500

    Task5

--- Tyuiu.VyazovES.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.VyazovES.Sprint0.Task5.V0/Program.cs
@@ -16,6 +16,7 @@ namespace Tyuiu.VyazovES.Sprint0.Task5.V0
             Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
             Console.WriteLine("A / B = " + DataService.Division(9, 3));
             Console.ReadKey();
+
         }
     }
 }
